3b7cdda089d5e6b86c34e480edd56985532ced11
Zana17-07 <rooz56930@gmail.com>
2022-10-12 17:02:14 +0200

    panier en cours

--- P5-Dev-Web-Kanap-master/front/JS/cart.js
+++ P5-Dev-Web-Kanap-master/front/JS/cart.js
@@ -3,7 +3,32 @@
 on récupère les données dans la page panier*/
 
 
+/*Initialiser la page cart.html*/
+kanapDisplay()
+
+/*Créer la fonction pour l'affichages des canapés*/
+function kanapDisplay() {
+
+/*Afficher le contenu du panier stocké dans le local storage*/
 
 
 
 
+
+/*utiliser la méthode Element.closet() pour cibler le produit en fonction
+de son id et sa couleur pour supprimer ou modifier une quantité avec
+addEventListener de type change*/
+
+
+
+
+
+/*Penser à bien modifier le DOM et le local storage pour conservées les modifications
+lors de rafraîchissement de la page ou de rechargement*/
+
+
+
+
+
+
+}
